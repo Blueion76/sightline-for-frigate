e343bf0d89964ea149e18c12b025883c0d0b23e8
Blueion <128919662+Blueion76@users.noreply.github.com>
2026-08-13 08:23:13 -0500

    Wire multiview recording modules

--- src/card/multi-recording.js
+++ src/card/multi-recording.js
@@ -1,1 +1,4 @@
-export const multiRecordingMethods = {};
+import { multiRecordingCoreMethods } from './multi-recording-core.js';
+import { multiRecordingPlayerMethods } from './multi-recording-player.js';
+import { multiRecordingControllerMethods } from './multi-recording-controller.js';
+export const multiRecordingMethods=Object.assign({},multiRecordingCoreMethods,multiRecordingPlayerMethods,multiRecordingControllerMethods);
