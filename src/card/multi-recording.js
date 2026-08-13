3fe412e1f38dfc6e6ea292930ce1f150523b46b8
Blueion <128919662+Blueion76@users.noreply.github.com>
2026-08-13 08:50:46 -0500

    Replace multiview method bundle with timeline UX fixes

--- src/card/multi-recording.js
+++
@@ -1,4 +0,0 @@
-import { multiRecordingCoreMethods } from './multi-recording-core.js';
-import { multiRecordingPlayerMethods } from './multi-recording-player.js';
-import { multiRecordingControllerMethods } from './multi-recording-controller.js';
-export const multiRecordingMethods=Object.assign({},multiRecordingCoreMethods,multiRecordingPlayerMethods,multiRecordingControllerMethods);
